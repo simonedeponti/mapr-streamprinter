0f76925fd2a2c58948e5dfbdf5320fd5d6a78b08
Simone Deponti <simone.deponti@gmail.com>
2017-10-27 15:00:19 +0200

    Updated to read from specific offset

--- src/main/scala/com/github/simonedeponti/maprstreamprinter/MaprStreamPrinter.scala
+++ src/main/scala/com/github/simonedeponti/maprstreamprinter/MaprStreamPrinter.scala
@@ -3,29 +3,42 @@ package com.github.simonedeponti.maprstreamprinter
 import java.util.Properties
 
 import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer}
+import org.apache.kafka.common.TopicPartition
 
 import scala.collection.JavaConverters._
 
 
 object MaprStreamPrinter {
 
-  def makeConsumer(args: Seq[String]): KafkaConsumer[String, String] = {
+  def makeConsumer(topics: Seq[String]): KafkaConsumer[String, String] = {
     val props: Properties = new Properties()
+    props.setProperty("group.id", "maprstream-printer")
+    props.setProperty("enable.auto.commit", "false")
     props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     val consumer = new KafkaConsumer[String, String](props)
-    val topics: Seq[String] = args.head.split(",").toSeq
     consumer.subscribe(topics.asJava)
     println(s"Subscribing to $topics")
     consumer
   }
 
   def main(args: Array[String]): Unit = {
-    val consumer = makeConsumer(args)
+    val consumer = makeConsumer(args.head.split(",").toSeq)
+    val offset: Option[(TopicPartition, Long)] = if (args.length > 1) {
+      var offset_components = args(1).split(":")
+      Some((new TopicPartition(offset_components.head, offset_components(1).toInt), offset_components.last.toLong))
+    }
+    else {
+      None
+    }
     val timeout = args.last.toInt * 1000
 
     try {
       var stop = false
+      if (offset.isDefined) {
+        consumer.poll(0)
+        consumer.seek(offset.get._1, offset.get._2)
+      }
       while(!stop) {
         val records: ConsumerRecords[String, String] = consumer.poll(timeout)
         val iterator: java.util.Iterator[ConsumerRecord[String, String]] = records.iterator()
