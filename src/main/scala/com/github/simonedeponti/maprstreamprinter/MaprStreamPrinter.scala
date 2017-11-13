b6c12251a733ecdaac9890962f506daf75c4ab90
Simone Deponti <simone.deponti@gmail.com>
2017-11-13 13:05:58 +0100

    Updated some offset handling

--- src/main/scala/com/github/simonedeponti/maprstreamprinter/MaprStreamPrinter.scala
+++ src/main/scala/com/github/simonedeponti/maprstreamprinter/MaprStreamPrinter.scala
@@ -10,15 +10,24 @@ import scala.collection.JavaConverters._
 
 object MaprStreamPrinter {
 
-  def makeConsumer(topics: Seq[String], withOffset: Boolean): KafkaConsumer[String, String] = {
+  def makeConsumer(topics: Seq[String], offset: Option[(TopicPartition, Long)]): KafkaConsumer[String, String] = {
     val props: Properties = new Properties()
+    props.setProperty("bootstrap.servers", "localhost:9092")
     props.setProperty("group.id", "maprstream-printer")
     props.setProperty("enable.auto.commit", "false")
     props.setProperty("auto.offset.reset", "earliest")
     props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     val consumer = new KafkaConsumer[String, String](props)
-    consumer.subscribe(topics.asJava)
+    if(offset.isDefined) {
+      consumer.assign(
+        Seq(offset.get._1).asJava
+      )
+      consumer.seek(offset.get._1, offset.get._2)
+    }
+    else {
+      consumer.subscribe(topics.asJava)
+    }
     println(s"### Subscribing to $topics\n\n")
     consumer
   }
@@ -31,15 +40,11 @@ object MaprStreamPrinter {
     else {
       None
     }
-    val consumer = makeConsumer(args.head.split(",").toSeq, offset.isDefined)
+    val consumer = makeConsumer(args.head.split(",").toSeq, offset)
     val timeout = args.last.toInt * 1000
 
     try {
       var stop = false
-      if (offset.isDefined) {
-        consumer.poll(0)
-        consumer.seek(offset.get._1, offset.get._2)
-      }
       while(!stop) {
         val records: ConsumerRecords[String, String] = consumer.poll(timeout)
         val iterator: java.util.Iterator[ConsumerRecord[String, String]] = records.iterator()
